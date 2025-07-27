73c1563a716a86b525eed3dd397f043a9247f3bf
kaustubh rane <kaustubh.anr@gmail.com>
2025-07-27 14:30:03 +0530

    Add routing and page components for Home, Login, Reset Password, and Email Verification

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import ResetPassword from './pages/ResetPassword'
+import verifyemail from './pages/verifyemail'
 
 const App = () => {
   return (
     <div>
-      
+      <Routes>
+      <Route path= '/' element={<Home/>}/>
+      <Route path= '/Login' element={<Login/>}/>
+      <Route path= '/Resetpassword' element={<ResetPassword/>}/>
+      <Route path= '/verifyemail' element={<verifyemail/>}/>
+
+      </Routes>
     </div>
   )
 }
---
+++ client/src/pages/verifyemail.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const verifyemail = () => {
+  return (
+    <div>
+      <h1>Welcome</h1>
+    </div>
+  )
+}
+
+export default verifyemail
