09bcbc0eda6165f8ff0df378196ab2a4c4c48a52
kaustubh rane <kaustubh.anr@gmail.com>
2025-07-27 16:14:43 +0530

    Refactor main application structure: integrate NavBar and Header components into Home page, and enhance layout with responsive design.

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import NavBar from '../components/NavBar'
+import Header from '../components/Header'
 const Home = () => {
   return (
-    <div>
-      <h1> Welcome to the react page</h1>
+    <div className=' flex flex-col items-center justify-center min-h-screen bg-[url("/bg_img.png")] bg-cover  bg-center '>
+      <NavBar/>
+      <Header/>
+
     </div>
   )
 }
