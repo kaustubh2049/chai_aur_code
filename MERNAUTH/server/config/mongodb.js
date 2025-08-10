2208021841afa3f7ee1108e20de1ac4f2e8c0d0e
kaustubh rane <kaustubh.anr@gmail.com>
2025-08-10 18:43:56 +0530

    Initialized a new Next.js Project

--- MERNAUTH/server/config/mongodb.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-const connectdb = async ()=>{
-    mongoose.connection.on('connected',()=>{
-        console.log("database connected")
-    })
-    await mongoose.connect(`${process.env.MONGODB_URL}/MERN-AUTH`);
-
-};
-export default connectdb;
--- MERNAUTH/server/routes/userroute.js
+++
@@ -1,6 +0,0 @@
-import express from 'express'
-import userauth from '../middleware/userauth.js';
-import { getuserdata } from '../controllers/usercontroller.js';
-const userrouter = express.Router();
-userrouter.get('/data',userauth,getuserdata)
-export default userrouter;
